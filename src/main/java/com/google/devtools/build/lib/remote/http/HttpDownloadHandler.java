3be80c00a3faf7f300b30efe2b3bde13f0861a23
Ilia K <ki.stfu@gmail.com>
2025-08-29 15:56:17 +0200

    DRAFT add zstd to accept encoding in HttpDownloadHandler#buildRequest (#23463)

--- src/main/java/com/google/devtools/build/lib/remote/http/HttpDownloadHandler.java
+++ src/main/java/com/google/devtools/build/lib/remote/http/HttpDownloadHandler.java
@@ -210,7 +210,7 @@ final class HttpDownloadHandler extends AbstractHttpHandler<HttpObject> {
     httpRequest.headers().set(HttpHeaderNames.HOST, host);
     httpRequest.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
     httpRequest.headers().set(HttpHeaderNames.ACCEPT, "*/*");
-    httpRequest.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
+    httpRequest.headers().set(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.ZSTD + ", " + HttpHeaderValues.GZIP);
     return httpRequest;
   }
 
